9ad4bf669cc5c74b472423084a4ed6eb4f1b730e
schererpatrick <schererpatrick91@gmail.com>
2023-03-07 23:12:35 +0100

    a

---
+++ chapter02/js/celsiustofahrenheit.js
@@ -0,0 +1,3 @@
+const celsius = prompt("Enter the temperature in Celsius degrees:");
+fahrenheit = celsius * 9/5 + 32;
+console.log(fahrenheit);
---
+++ chapter02/js/vatcalculation.js
@@ -0,0 +1,3 @@
+const rawprice = prompt("Enter the raw price:");
+finalprice = rawprice * 1.2;
+console.log(finalprice);
