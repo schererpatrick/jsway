fb491a95767ba255beac5ce6947e31d09669b45d
schererpatrick <schererpatrick91@gmail.com>
2023-03-08 14:52:37 +0100

    a

---
+++ chapter03/js/numbercomparison.js
@@ -0,0 +1,14 @@
+const nb1 = Number(prompt("Enter first number:"));
+const nb2 = Number(prompt("Enter second number:"));
+
+if ( nb1 === nb2 ) {
+    console.log("numbers are equal")
+}
+
+if ( nb1 > nb 2) {
+    console.log("number 1 is greater")
+}
+
+if ( nb 1 < nb 2) {
+    console.log("number 2 is greater")
+}
