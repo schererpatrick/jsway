d31ac795ff89a06a4a67d658b2744a2f1307e967
schererpatrick <schererpatrick91@gmail.com>
2023-03-08 11:31:47 +0100

    a

---
+++ chapter03/js/weather1.js
@@ -0,0 +1,12 @@
+const weather = prompt("What's the weather like?");
+if (weather === "sunny") {
+console.log("T-shirt time!");
+} else if (weather === "windy") {
+console.log("Windbreaker life.");
+} else if (weather === "rainy") {
+console.log("Bring that umbrella!");
+} else if (weather === "snowy") {
+console.log("Just stay inside!");
+} else {
+console.log("Not a valid weather type");
+}
