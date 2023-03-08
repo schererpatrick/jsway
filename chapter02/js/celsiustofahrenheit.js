11a265f499ac08dd519f1574105c212508a005c2
schererpatrick <schererpatrick91@gmail.com>
2023-03-08 10:34:44 +0100

    a

--- chapter02/js/celsiustofahrenheit.js
+++ chapter02/js/celsiustofahrenheit.js
@@ -1,3 +1,3 @@
-const celsius = prompt("Enter the temperature in Celsius degrees:");
-fahrenheit = celsius * 9/5 + 32;
-console.log(fahrenheit);
+const celsius = Number(prompt("Enter the temperature in Celsius degrees:"));
+fahrenheit = celsius * 9 / 5 + 32;
+console.log(`${celsius}°C = ${fahrenheit}°F`);
--- chapter02/js/vatcalculation.js
+++ chapter02/js/vatcalculation.js
@@ -1,3 +1,5 @@
-const rawprice = prompt("Enter the raw price:");
-finalprice = rawprice * 1.2;
-console.log(finalprice);
+const rawPrice = Number(prompt("Enter the raw price:"));
+// VAT rate = 20.6%
+const vatRate = 20.6 / 100;
+const finalPrice = rawPrice * (1 + vatRate);
+console.log(`The final price is ${finalPrice}`);
