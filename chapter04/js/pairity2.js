31fe7fea2fb72d66260a68d7ecfa34c280b9d1b1
schererpatrick <schererpatrick91@gmail.com>
2023-03-09 19:10:33 +0100

    a

--- chapter04/js/pairity2.js
+++ chapter04/js/pairity2.js
@@ -2,7 +2,7 @@ const number = Number(prompt("Enter number to start?"));
 
 
 
-for (let i = Number; i <= Number + 10; i++) {
+for (let i = number; i <= Number + 10; i++) {
     if (i % 2 === 0) {
     console.log(`${i} is even`);
     }
