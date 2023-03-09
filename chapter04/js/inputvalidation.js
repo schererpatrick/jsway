36e79f77fd34d3adaf8b73667bc05c056813ea0a
schererpatrick <schererpatrick91@gmail.com>
2023-03-09 19:41:10 +0100

    a

--- chapter04/js/inputvalidation.js
+++ chapter04/js/inputvalidation.js
@@ -1,4 +1,4 @@
 let number = 0;
-while (number < 100) {
-    number = Number(prompt("Enter a number less than or equal to 100:"));
+while (number < 50 || number > 100) {
+  number = Number(prompt("Enter a number between 50 and 100:"));
 }
