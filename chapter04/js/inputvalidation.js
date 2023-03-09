b3b6839d0a7acefd4b0505d82b51900c0495dad8
schererpatrick <schererpatrick91@gmail.com>
2023-03-09 19:37:17 +0100

    a

---
+++ chapter04/js/inputvalidation.js
@@ -0,0 +1,7 @@
+const number = Number(prompt("Enter a number less than or equal to 100:"));
+
+
+let i = number;
+while (i < 100) {
+    Number(prompt("Enter a number less than or equal to 100:"));
+}
